bc378c4241e1e2d082930ee8590a41a45cae39cb
Matt Ruttan <matthew.ruttan5@gmail.com>
2016-12-15 02:56:11 +0000

    Make the request Step 5

--- download_avatars.js
+++ download_avatars.js
@@ -9,6 +9,26 @@ var GITHUB_TOKEN = "a33de2e8c4885a80071d2ea24c7efea3e94337ba";
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
   console.log(requestURL);
+
+  var option = {
+  	headers: {'user-agent': 'GitHub Avatar Downloader'}
+  };
+  var body = '';
+  
+  request.get(requestURL, option)
+  		.on('error', function (err) {
+			throw err;
+		})
+		.on('response', function(response) {
+			console.log('Response Status Code: ', response.statusCode);
+			response.setEncoding('utf8')
+					.on('data', function(data) {
+						body += data;
+					});
+		})
+		.on('end', function() {
+			console.log("Body: " + body);
+		})
 }
 
 getRepoContributors("jquery", "jquery", function(err,result){
