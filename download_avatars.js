d0bfdbd064568cc12a3329e78ee6e1cf2eccaa77
Matt Ruttan <matthew.ruttan5@gmail.com>
2016-12-15 02:38:09 +0000

    Create the requestURL Step 4

--- download_avatars.js
+++ download_avatars.js
@@ -2,9 +2,13 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+var GITHUB_USER = "mruttan";
+var GITHUB_TOKEN = "a33de2e8c4885a80071d2ea24c7efea3e94337ba";
+
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
 }
 
 getRepoContributors("jquery", "jquery", function(err,result){
